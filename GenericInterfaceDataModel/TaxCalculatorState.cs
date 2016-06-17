2a96051b33c97a03b7756de9f2743cc0a1dd173a
somubr <somnath.87@gmail.com>
2016-06-17 16:59:45 +0530

    spring .net demo

---
+++ GenericInterfaceDataModel/TaxCalculatorState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericInterfaceDataModel
+{
+    public class TaxCalculatorState<T,t>:ITaxRepository<T,t>
+    {
+        public t GetVat(T request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenericInterfaceDependencyInjection/Program.cs
@@ -0,0 +1,26 @@
+﻿using GenericInterfaceDataModel;
+using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericInterfaceDependencyInjection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IApplicationContext applicationContext = ContextRegistry.GetContext();
+
+
+            var vat = (ITaxRepository<StateRequest, StateResponse>)applicationContext["TaxCalculatorState"];
+            //container.Res
+
+            Console.WriteLine(vat.GetVat(new StateRequest { TaxRate = 12 }));
+            Console.ReadLine();
+        }
+    }
+}
